6743204371e0fd719dc698f02d7b68d3b9b44bbf
Fernando Dorantes <fdorantesm@gmail.com>
2023-02-02 19:32:29 -0600

    refactor: add metadata into users seeder

--- database/seeds/user.seeder.ts
+++ database/seeds/user.seeder.ts
@@ -1,29 +1,46 @@
 import { UserEntity } from 'apps/users/src/infrastructure/domain/user.entity';
 import { DataSource } from 'typeorm';
 import { PromisePool } from '@supercharge/promise-pool';
-import { Seeder, SeederFactoryManager } from 'typeorm-extension';
+import { Seeder } from 'typeorm-extension';
 import * as faker from 'faker';
 import { User } from 'apps/users/src/domain/user';
+import { UserMetaDataEntity } from 'apps/users/src/infrastructure/domain/user-metadata.entity';
+import { UserMetaData } from 'apps/users/src/domain/user-metadata';
 
 export default class UserSeeder implements Seeder {
   public async run(dataSource: DataSource): Promise<void> {
-    const repository = dataSource.getRepository(UserEntity);
-    const users = Array.from(Array(10000).keys()).map(() => {
+    const userRepository = dataSource.getRepository(UserEntity);
+    const userMetaDataRepository = dataSource.getRepository(UserMetaDataEntity);
+
+    const users = Array.from(Array(100).keys()).map(() => {
       const firstName = faker.name.firstName();
       const lastName = faker.name.lastName();
+      const image = faker.internet.avatar();
+      const birthday = faker.date.past(65);
       return {
         name: `${firstName} ${lastName}`,
         email: faker.internet.email(firstName, lastName).toLowerCase(),
-        bio: faker.lorem.sentence(),
+        metadata: {
+          image,
+          birthday,
+        },
       };
     });
 
     try {
       await PromisePool.withConcurrency(10)
         .for(users)
-        .process(async (userData) => {
-          const user = new User(userData);
-          await repository.insert(user.entityRoot());
+        .process(async (person) => {
+          const user = new User({ name: person.name, email: person.email });
+          const {
+            raw: [result],
+          } = await userRepository.insert(user.entityRoot());
+          const userMetaData = new UserMetaData({
+            image: person.metadata.image,
+            birthday: person.metadata.birthday,
+            userId: result.id,
+          });
+          await userMetaDataRepository.insert(userMetaData.entityRoot());
         });
     } catch (error) {
       console.error('UserSeeder Error', error);
