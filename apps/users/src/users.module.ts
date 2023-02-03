b2ce26c9a30161194cb85962e28ce580ee61fd68
Fernando Dorantes <fdorantesm@gmail.com>
2023-02-03 06:54:23 -0600

    fix: import user metadata entity into users module

--- apps/users/src/users.module.ts
+++ apps/users/src/users.module.ts
@@ -3,7 +3,10 @@ import { ConfigModule } from '@app/config/config.module';
 import { options } from '@app/config/options/config.options';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { USER_REPOSITORY } from './application/constants/injection-tokens';
+import {
+  USER_METADATA_REPOSITORY,
+  USER_REPOSITORY,
+} from './application/constants/injection-tokens';
 import { CreateUserService } from './application/create-user/create-user.service';
 import { DeleteUserService } from './application/delete-user/delete-user.service';
 import { GetUserService } from './application/get-user/get-user.service';
@@ -12,11 +15,13 @@ import { UserController } from './infrastructure/controllers/user.controller';
 import { UsersController } from './infrastructure/controllers/users.controller';
 import { UserEntity } from './infrastructure/domain/user.entity';
 import { UserRepository } from './infrastructure/repositories/user.repository';
+import { UserMetaDataRepository } from './infrastructure/repositories/user-metadata.repository';
+import { UserMetaDataEntity } from './infrastructure/domain/user-metadata.entity';
 
 @Module({
   imports: [
     ConfigModule.forRoot(options),
-    TypeOrmModule.forFeature([UserEntity]),
+    TypeOrmModule.forFeature([UserEntity, UserMetaDataEntity]),
     DatabaseModule,
   ],
   controllers: [UserController, UsersController],
@@ -29,7 +34,11 @@ import { UserRepository } from './infrastructure/repositories/user.repository';
       provide: USER_REPOSITORY,
       useClass: UserRepository,
     },
+    {
+      provide: USER_METADATA_REPOSITORY,
+      useClass: UserMetaDataRepository,
+    },
   ],
-  exports: [USER_REPOSITORY],
+  exports: [USER_REPOSITORY, USER_METADATA_REPOSITORY],
 })
 export class UsersModule {}
